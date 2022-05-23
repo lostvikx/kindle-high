5a4c81afcb16efed9704e58154ffce27274df343
Vikram Negi <viknegi0@gmail.com>
2022-05-23 16:54:42 +0530

    login

--- scraper.py
+++ scraper.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from getpass import getpass
 
 def user_auth():
@@ -11,9 +13,14 @@ def user_auth():
 def login(web):
   user_login = user_auth()
 
-  email = web.find_element_by_id("ap_email")
+  email = web.find_element(By.ID, "ap_email")
   email.send_keys(user_login[0])
-  password = web.find_element_by_id("ap_password")
+
+  password = web.find_element(By.ID, "ap_password")
+  password.send_keys(user_login[1])
+
+  sign_in_btn = web.find_element(By.ID, "signInSubmit")
+  sign_in_btn.click()
 
 def main():
   browser = webdriver.Firefox()
