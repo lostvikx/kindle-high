a27a523c84f92e6bbf5566ed5a64c1873c957698
Vikram Negi <viknegi0@gmail.com>
2022-05-24 14:23:18 +0530

    prompt a random annotation

--- prompt.py
+++ prompt.py
@@ -8,11 +8,19 @@ def main():
 
   with open(highlights_save_file_name, "r", encoding="utf-8") as f:
     data = json.load(f)
-    books = list(data.keys())
-    selected_book = choice(books)
+    all_annotations = []
 
-    high = choice(data[selected_book])
-    print(f"{high} ~ {selected_book}")
+    for book_name, highlights in data.items():
+
+      for high in highlights:
+        all_annotations.append(f"{high} ~ {book_name}")
+
+    # books = list(data.keys())
+    # selected_book = choice(books)
+
+    # high = choice(data[selected_book])
+    # print(f"{high} ~ {selected_book}")
+    print(choice(all_annotations))
 
 if __name__ == "__main__":
   main()
