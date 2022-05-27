bf9ee900d0dd1c21340379cd3e896362fd4a18d5
Vikram Negi <viknegi0@gmail.com>
2022-05-27 23:28:15 +0530

    search algo

--- print.py
+++ print.py
@@ -6,28 +6,46 @@ from random import choice
 import subprocess
 import os
 from dotenv import load_dotenv, find_dotenv
+import re
 
 
 def parse_highlights(book_highlights):
-  pass
-
-
-def select_annotation(book_highlights)->str:
   """
-  Select a random highlight
+  Parses highlights into all_annotations and book_names
 
   Args:
     book_highlights: { "book_name": [ ...highlights ] }
 
   Returns:
-    Random annotation, a string, "highlight ~ book_name"
+    Random (all_annotation: [ ..."highlight ~ book_name" ], book_names: [ ...book_name ])
   """
+
   all_annotations = []
+  book_names = list(book_highlights.keys())
+
   for book_name, highlights in book_highlights.items():
     for high in highlights:
       all_annotations.append(f"{high} ~ {book_name}")
 
-  return choice(all_annotations)
+  return (all_annotations, book_names)
+
+
+# def select_annotation(book_highlights)->str:
+#   """
+#   Select a random highlight
+
+#   Args:
+#     book_highlights: { "book_name": [ ...highlights ] }
+
+#   Returns:
+#     Random annotation, a string, "highlight ~ book_name"
+#   """
+#   all_annotations = []
+#   for book_name, highlights in book_highlights.items():
+#     for high in highlights:
+#       all_annotations.append(f"{high} ~ {book_name}")
+
+#   return choice(all_annotations)
 
 def parse_args():
   """
@@ -38,9 +56,9 @@ def parse_args():
   """
   parser = ArgumentParser(description="Print a random annotation from your kindle highlights")
 
-  parser.add_argument("-u", "--update-highlights", help="Update the annotations by running fetch.py", action="store_true")
+  parser.add_argument("-u", "--update-highlights", help="update the annotations by running fetch.py", action="store_true")
 
-  # parser.add_argument("--book-name", help="get annotation from specific book", choices=())
+  parser.add_argument("-b", "--book-name", help="get annotation from specific book")
 
   args = parser.parse_args()
   return args
@@ -58,7 +76,9 @@ def main():
   try:
     with open(highlights_save_file_name, "r", encoding="utf-8") as f:
       data = json.load(f)
-      annotation = select_annotation(book_highlights=data)
+      # annotation = select_annotation(book_highlights=data)
+      all_annotations, book_names = parse_highlights(book_highlights=data)
+
   except FileNotFoundError:
     print(f"{highlights_save_file_name} not found!")
     needs_fetch = True
@@ -68,12 +88,29 @@ def main():
 
   if parse_args().update_highlights or needs_fetch:
     print("Updating highlights.json...")
-    subprocess.run(["python3", "fetch.py"])
-  
-  try:
-    print(annotation)
-  except Exception as err:
-    print(f"Couldn't print highlight: {err}")
+    # try:
+    #   subprocess.run(["python3", "fetch.py"])
+    # except Exception as err:
+    #   print(f"Couldn't run fetch.py: {err}")
+
+  elif parse_args().book_name:
+    book_name = parse_args().book_name
+
+    matches = []
+    for name in book_names:
+      if re.search(book_name, name, re.I):
+        matches.append(name)
+
+    print(matches)
+    if len(matches) == 1:
+      pass
+
+  else:
+    annotation = choice(all_annotations)
+    try:
+      print(annotation)
+    except Exception as err:
+      print(f"Couldn't print highlight: {err}")
 
 
 if __name__ == "__main__":
